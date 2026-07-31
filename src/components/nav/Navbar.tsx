98c7816285e0aa79b83100dde8e2c0ac260ebe01
WelleCFX <wellecfx@gmail.com>
2026-07-31 14:28:27 +0300

    Fix keyboard bug in settings, admin manual plan assignment, rename Dashboard to Моят магазин

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -203,7 +203,7 @@ export function Navbar() {
                             className="dropdown-item"
                           >
                             <LayoutDashboard size={15} />
-                            Dashboard
+                            Моят магазин
                           </Link>
                         )}
 
